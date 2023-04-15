315bfdd80c6bc40738269f75a4f779b432eaf30f
superhitishagithub <129837958+superhitishagithub@users.noreply.github.com>
2023-04-15 15:59:19 +0530

    Add files via upload

---
+++ random.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[108]:
+
+
+import random
+
+hand = ["rock","paper","scissor"]
+player1 = random.choice(hand)
+print(player1)
+
+
+
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
